5202c208366bbb039cba1f09b4341f62ee4a46ff
K-h-a-l-e-d <mechanic1080p@gmail.com>
2026-07-26 19:41:56 +0300

    feat: basic express server running

---
+++ server.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "Tasks Manager API is running." });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`),
+);
