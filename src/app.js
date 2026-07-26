ce4f3abe39b004beb972aa8a058532b6b05bf7ec
K-h-a-l-e-d <mechanic1080p@gmail.com>
2026-07-26 22:33:23 +0300

    feat: wire routes into app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 
+const tasksRoutes = require("./routes/taskRoutes");
+const authRoutes = require("./routes/authRoutes");
+const { notFound, errorHandler } = require("./middleware/errorHandler");
+
 app.get("/", (req, res) => {
   res.json({ success: true, message: "Tasks Manager API is running." });
 });
+
+// Routes
+app.use("/tasks", tasksRoutes);
+app.use("/login", authRoutes);
+
+// 404 + error handlers
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
